40ae26d784f12c5967f4966add301a3a31872ca2
DDRIV <ddriv4@gmail.com>
2021-10-13 10:27:20 -0500

    Finished Java and JS problems

--- 10_11_2021/JavaAnswers.java
+++ 10_11_2021/JavaAnswers.java
@@ -1,39 +1,59 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
 
 public class JavaAnswers {
 
-    /*
-    1) Write a Java Program to iterate HashMap using While and advance for loop.
 
-    2) Write a program which accepts two given String and checks if they are the rotation of each. 
-    If they then return true otherwise return false. 
-    A String is said to be a rotation of other string if they contain same characters 
+    // 1) Write a Java Program to iterate HashMap using While and advance for loop.
+
+    public static void whileIterateHashMap(HashMap hm) {
+        Object[] hms = hm.keySet().toArray();
+        int i = 0;
+        while(i < hms.length) {
+            Object o = hm.get(hms[i]);
+
+            // insert logic
+
+            i++;
+        }
+    }
+
+    public static void advancedForLoopIterateHashMap(HashMap hm) {
+        Set hms = hm.keySet();
+        for(Object o:hms) {
+            Object obj = hm.get(o);
+
+            //insert logic here
+        }
+    }
+
+    /*
+    2) Write a program which accepts two given String and checks if they are the rotation of each.
+    If they then return true otherwise return false.
+    A String is said to be a rotation of other string if they contain same characters
     and the sequence is rotated across any character, e.g. "dabc" is a rotation of "abcd" but "dbac" is not.
     */
 
-    public static boolean validateCard(String id){
-        //String idStr = Long.toString(id);
+    public static boolean stringRotation(String str1, String str2) {
 
-        if(id.length() < 14 || id.length() > 19){
-            return false;
-        }
+        if(str1.length() != str2.length()) return false;
 
-        int[] ints = new int[id.length()-1];
-        for(int i = 0; i < ints.length; i++){
-            int in = id.length()-i-2;
-            int k = Integer.parseInt(id.substring(in, in+1));
-
-            if((i+1)%2 != 0){
-                if(k*2 >= 10) {
-                    ints[i] = 1 + (k*2)%10;
-                } else {
-                    ints[i] = k*2;
-                }
-            } else {
-                ints[i] = k;
+        for(int i = 0; i < str1.length(); i++){
+            if(str2.contains(str1.substring(0,i)) && str2.contains(str1.substring(i,str1.length()-1))) {
+                return true;
             }
         }
 
-        return Integer.parseInt(id.substring(id.length()-1)) == (10 - (Arrays.stream(ints).sum() % 10));
+        for(int i = 0; i < str1.length(); i++){
+            if(str1.contains(str2.substring(0,i)) && str1.contains(str2.substring(i,str1.length()-1))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- 10_11_2021/JavaScriptAnswers.js
+++ 10_11_2021/JavaScriptAnswers.js
@@ -1,4 +1,7 @@
-// 1) Write a function that takes a string as an argument and returns a list of all the words inflected by "-ing". Your function should also exclude all the mono-syllabic words ending in "-ing" (e.g. bing, sing, sling, ...). Although these words end in "-ing", the "-ing" is not an inflection affix.
+// 1) Write a function that takes a string as an argument and returns a list of all the 
+//      words inflected by "-ing". Your function should also exclude all the mono-syllabic 
+//      words ending in "-ing" (e.g. bing, sing, sling, ...). Although these words end 
+//      in "-ing", the "-ing" is not an inflection affix.
 
 // Examples
 // ingExtractor("coming bringing Letting sing") --> ["coming", "bringing", "Letting"]
@@ -12,12 +15,46 @@
 
 function ingExtractor(a) {
     let string = a;
-    
+    const ing = "ing";
+    const str = string.split(" ");
+    let result = [];
+    for(let s of str){
+        if(s.toLowerCase().endsWith(ing)) {
+            let front = s.substring(0,s.length-3).toLowerCase();
+            if(front.contains("a")) {
+                result.push(s);
+                continue;
+            }
+            if(front.contains("e")) {
+                result.push(s);
+                continue;
+            }
+            if(front.contains("i")) {
+                result.push(s);
+                continue;
+            }
+            if(front.contains("o")) {
+                result.push(s);
+                continue;
+            }
+            if(front.contains("u")) {
+                result.push(s);
+                continue;
+            }
+            if(front.contains("y")) {
+                result.push(s);
+                continue;
+            }
+        }
+    }
+    return result;
 }
 
 // 2) Your task is to create a function that simulates a vending machine.
 
-// Given an amount of money (in cents ¢ to make it simpler) and a productNumber, the vending machine should output the correct product name and give back the correct amount of change.
+// Given an amount of money (in cents ¢ to make it simpler) and a productNumber, 
+// the vending machine should output the correct product name and give back 
+// the correct amount of change.
 
 // The coins used for the change are the following: [500, 200, 100, 50, 20, 10]
 
@@ -33,7 +70,75 @@ function ingExtractor(a) {
 
 // Notes:
 // - The products are fixed and you can find them in the Tests tab.
-// - If productNumber is invalid (out of range) you should return the string: "Enter a valid product number".
-// - If money is not enough to buy a certain product you should return the string: "Not enough money for this product".
+// - If productNumber is invalid (out of range) you should return the 
+//      string: "Enter a valid product number".
+// - If money is not enough to buy a certain product you should return the 
+//      string: "Not enough money for this product".
 // - If there's no change, return an empty array as the change.
 
+function vendingMachine(products, money, productNumber){
+    let coins = [500, 200, 100, 50, 20, 10];
+    let product;
+    for(let p of products){
+        if(p.productNumber === productNumber){
+            product = p;
+            break;
+        }
+    }
+    if(product == undefined){
+        return "Enter a valid product number"
+    }
+    if (product.cost > money){
+        return "Not enough money for this product";
+    }
+    
+    let input = money;
+    let change = [];
+    while(input > 0){
+        if(input >= 500){
+            change.push(500);
+            input -= 500
+            continue;
+        }
+        if(input >= 200){
+            change.push(200)
+            input -= 200
+            continue
+        }
+        if(input >= 100){
+            change.push(100)
+            input -= 100
+            continue;
+        }
+        if(input >= 50){
+            change.push(50)
+            input -= 50
+            continue;
+        }
+        if(input >= 20){
+            change.push(20)
+            input -= 20
+            continue;
+        }
+        if(input >= 10){
+            change.push(10)
+            input -= 10
+            continue;
+        }
+        else{
+            change.push(input)
+            input = 0
+        }
+    }
+}
+
+let products = [
+    {
+        productNumber:1,
+        cost:200
+    },
+    {
+        productNumber:2,
+        cost:150
+    }
+]
